76b54ebd851166fa8784cc0612438f20dbf4b4f5
Jesse Tane <jesse.tane@gmail.com>
2016-02-29 23:12:44 -0500

    implement new features

--- index.js
+++ index.js
@@ -10,23 +10,64 @@ function RPC () {
 RPC.prototype.call = function (name) {
   var message = { method: name }
   var params = Array.prototype.slice.call(arguments, 1)
-  var cb = params.slice(-1)[0]
+  var id, cb = params.slice(-1)[0]
   if (typeof cb === 'function') {
-    message.id = String(this._callback++)
-    this._callbacks[message.id] = cb
+    id = message.id = this._callback++
+    this._callbacks[id] = cb
     params.pop()
   }
   if (params.length) {
-    message.params = params
+    message.params = this.objectMode ? params[0] : params
   }
-  this.send(this.serialize ? this.serialize(message) : message)
+  if (cb && this.timeout) {
+    setTimeout(function () {
+      if (this._callbacks[id]) {
+        delete this._callbacks[id]
+        var err = new Error('Call timed out')
+        err.code = -32603
+        cb(err)
+      }
+    }.bind(this), this.timeout)
+  }
+  this._dosend(message, id)
+}
+
+RPC.prototype._dosend = function (message, cbid) {
+  if (this.serialize) {
+    message = this.serialize(message)
+  }
+  try {
+    this.send(message)
+  } catch (err) {
+    if (cbid !== undefined) {
+      var cb = this._callbacks[cbid]
+      delete this._callbacks[cbid]
+      err.code = -32603
+      cb(err)
+    }
+  }
 }
 
 RPC.prototype.onmessage = function (message) {
-  message = this.deserialize ? this.deserialize(message) : message
-  var method = message.method
-  if (method) {
-    this._handleRequest(method, message)
+  if (this.deserialize) {
+    try {
+      message = this.deserialize(message)
+    } catch (err) {
+      if (message.id) {
+        this._dosend({
+          id: message.id,
+          error: {
+            code: -32700,
+            message: 'Parse error'
+          }
+        })
+      }
+      return
+    }
+  }
+  var name = message.method
+  if (name) {
+    this._handleRequest(name, message)
   } else {
     this._handleResponse(message)
   }
@@ -35,37 +76,45 @@ RPC.prototype.onmessage = function (message) {
 RPC.prototype._handleRequest = function (name, message) {
   var id = message.id
   var params = message.params
+  if (!Array.isArray(params)) {
+    params = params === undefined ? [] : [ params ]
+  }
   var method = this.methods[name]
-  if (method) {
-    if (id) {
-      var self = this
-      method.apply(null, (params || []).concat(function (err) {
-        if (err) {
-          err = {
-            message: err.message,
-            code: err.code,
-            data: err.data
-          }
-        }
-        message = {
-          id: id,
-          error: err,
-          results: Array.prototype.slice.call(arguments, 1)
-        }
-        self.send(self.serialize ? self.serialize(message) : message)
-      }))
-    } else {
+  if (!method && this.defaultMethod) {
+    params.unshift(name)
+    method = this.defaultMethod
+  }
+  if (id === undefined) {
+    if (method) {
       method.apply(null, params)
     }
-  } else if (id) {
-    message = {
+  } else if (method) {
+    var self = this
+    method.apply(null, params.concat(function (err) {
+      if (err) {
+        err = {
+          message: err.message,
+          code: err.code,
+          data: err.data
+        }
+      }
+      message = {
+        id: id,
+        error: err
+      }
+      if (arguments.length > 1) {
+        message.result = self.objectMode ? arguments[1] : Array.prototype.slice.call(arguments, 1)
+      }
+      self._dosend(message)
+    }))
+  } else {
+    this._dosend({
       id: id,
       error: {
         code: -32601,
         message: 'Method not found'
       }
-    }
-    this.send(this.serialize ? this.serialize(message) : message)
+    })
   }
 }
 
@@ -81,6 +130,6 @@ RPC.prototype._handleResponse = function (message) {
       err.code = error.code
       err.data = error.data
     }
-    cb.apply(null, [err].concat(message.results))
+    cb.apply(null, [err].concat(message.result))
   }
 }
