0aa5258ce625ada16c87c6618afca1315c009fa3
Jesse Tane <jesse.tane@gmail.com>
2020-07-26 01:07:00 -0400

    update test

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
-var tape = require('tape')
-var Rpc = require('./')
+import Rpc from 'rpc-engine/index.js'
+import tap from 'tap-esm/index.js'
 
 var a = new Rpc()
 a.setInterface({
@@ -25,41 +25,41 @@ b.send = function (message) {
   })
 }
 
-tape('get and set interfaces by path', function (t) {
+tap('get and set interfaces by path', function (t) {
   t.plan(5)
   var defaultInterface = a.getInterface()
   t.equal(a.getInterface(''), defaultInterface)
-  a.on('interface-remove', handler)
+  a.addEventListener('interface-remove', handler)
   a.setInterface()
   t.equal(a.getInterface(), defaultInterface)
   a.setInterface('', defaultInterface)
   a.setInterface('')
-  a.removeListener('interface-remove', handler)
-  function handler (iface, path) {
-    t.equal(iface, defaultInterface)
+  a.removeEventListener('interface-remove', handler)
+  function handler (evt) {
+    t.equal(evt.detail.iface, defaultInterface)
   }
   t.equal(a.getInterface(), undefined)
   a.setInterface(defaultInterface)
   t.equal(a.getInterface(), defaultInterface)
 })
 
-tape('call remote method on b without params', function (t) {
+tap('call remote method on b without params', function (t) {
   t.plan(2)
   a.call('hello', function (err, result) {
-    t.error(err)
+    t.notOk(err)
     t.equal(result, 'world')
   })
 })
 
-tape('call remote method on a with params', function (t) {
+tap('call remote method on a with params', function (t) {
   t.plan(2)
   b.call('add', 1, 2, function (err, result) {
-    t.error(err)
+    t.notOk(err)
     t.equal(result, 3)
   })
 })
 
-tape('call remote method on subinterface of b', function (t) {
+tap('call remote method on subinterface of b', function (t) {
   t.plan(4)
   b.setInterface('sub', {
     test: function (param, cb) {
@@ -68,19 +68,19 @@ tape('call remote method on subinterface of b', function (t) {
     }
   })
   a.call('sub.test', 42, function (err) {
-    t.error(err)
+    t.notOk(err)
     t.equal(typeof b.getInterface('sub').test, 'function')
     b.setInterface('sub', null)
     t.equal(b.getInterface('sub'), undefined)
   })
 })
 
-tape('serialize and deserialize', function (t) {
+tap('serialize and deserialize', function (t) {
   t.plan(2)
   a.serialize = b.serialize = JSON.stringify
   a.deserialize = b.deserialize = JSON.parse
   b.call('add', 41, 1, function (err, result) {
-    t.error(err)
+    t.notOk(err)
     t.equal(result, 42)
     delete a.serialize
     delete a.deserialize
@@ -89,19 +89,19 @@ tape('serialize and deserialize', function (t) {
   })
 })
 
-tape('send a notification to b', function (t) {
+tap('send a notification to b', function (t) {
   t.plan(2)
   b.getInterface().notify = function (notification) {
     t.equal(notification, 'alert')
   }
-  b.once('notify', function (evt) {
-    t.equal(evt, 'alert')
+  b.addEventListener('notify', function (evt) {
+    t.equal(evt.detail[0], 'alert')
     delete b.getInterface().notify
-  })
+  }, { once: true })
   a.call('notify', 'alert')
 })
 
-tape('return not found error for missing method', function (t) {
+tap('return not found error for missing method', function (t) {
   t.plan(2)
   a.call('bogus', function (err) {
     t.equal(err.code, -32601)
@@ -109,7 +109,7 @@ tape('return not found error for missing method', function (t) {
   })
 })
 
-tape('invoke methods in the correct context', function (t) {
+tap('invoke methods in the correct context', function (t) {
   t.plan(4)
   var iface = {
     unbound: function (cb) {
@@ -141,7 +141,7 @@ tape('invoke methods in the correct context', function (t) {
   })
 })
 
-tape('invoke defaultMethod (if available) for unknown methods', function (t) {
+tap('invoke defaultMethod (if available) for unknown methods', function (t) {
   t.plan(3)
   b.defaultMethod = function (name, x, cb) {
     t.equal(name, 'unknown')
@@ -149,12 +149,12 @@ tape('invoke defaultMethod (if available) for unknown methods', function (t) {
     cb()
   }
   a.call('unknown', 42, function (err) {
-    t.error(err)
+    t.notOk(err)
     delete b.defaultMethod
   })
 })
 
-tape('invoke defaultMethod (if available) for unknown notifications', function (t) {
+tap('invoke defaultMethod (if available) for unknown notifications', function (t) {
   t.plan(2)
   b.defaultMethod = function (name, x) {
     t.equal(name, 'unknown')
@@ -164,7 +164,7 @@ tape('invoke defaultMethod (if available) for unknown notifications', function (
   a.call('unknown', 42)
 })
 
-tape('timeout calls if specified', function (t) {
+tap('timeout calls if specified', function (t) {
   t.plan(2)
   var timeout = null
   a.timeout = 50
@@ -180,7 +180,7 @@ tape('timeout calls if specified', function (t) {
   })
 })
 
-tape('use object-based params / result by setting objectMode', function (t) {
+tap('use object-based params / result by setting objectMode', function (t) {
   t.plan(3)
   a.objectMode = b.objectMode = true
   b.getInterface().question = function (params, cb) {
@@ -188,7 +188,7 @@ tape('use object-based params / result by setting objectMode', function (t) {
     cb(null, { answer: 42 })
   }
   a.call('question', { question: 'universe' }, function (err, result) {
-    t.error(err)
+    t.notOk(err)
     t.equal(result.answer, 42)
     delete a.objectMode
     delete b.objectMode
@@ -196,24 +196,25 @@ tape('use object-based params / result by setting objectMode', function (t) {
   })
 })
 
-tape('respond to parse errors', function (t) {
+tap('respond to parse errors', function (t) {
   t.plan(2)
   a.deserialize = JSON.parse
   b.serialize = function () {
     return undefined
   }
-  b.on('error', onerror)
-  function onerror (err) {
+  b.addEventListener('error', onerror)
+  function onerror (evt) {
+    var err = evt.detail
     t.equal(err.code, -32700)
     t.equal(err.message, 'Parse error')
     delete a.deserialize
     delete b.serialize
-    b.removeListener('error', onerror)
+    b.removeEventListener('error', onerror)
   }
   b.call('add', 1, 2, t.fail)
 })
 
-tape('respond to parse errors via callback if possible', function (t) {
+tap('respond to parse errors via callback if possible', function (t) {
   t.plan(2)
   a.deserialize = JSON.parse
   b.call('add', 1, 2, function (err) {
@@ -223,7 +224,7 @@ tape('respond to parse errors via callback if possible', function (t) {
   })
 })
 
-tape('catch send errors for method calls', function (t) {
+tap('catch send errors for method calls', function (t) {
   t.plan(2)
   var oldSend = b.send
   b.send = function () {
@@ -236,28 +237,30 @@ tape('catch send errors for method calls', function (t) {
   })
 })
 
-tape('catch send errors for notifications when error handler is present', function (t) {
+tap('catch send errors for notifications when error handler is present', function (t) {
   t.plan(2)
   var oldSend = b.send
   b.send = function () {
     throw new Error('Send failed')
   }
-  b.on('error', onerror)
-  function onerror (err) {
+  b.addEventListener('error', onerror)
+  function onerror (evt) {
+    var err = evt.detail
     t.equal(err.code, -32603)
     t.equal(err.message, 'Send failed')
     b.send = oldSend
-    b.removeListener('error', onerror)
+    b.removeEventListener('error', onerror)
   }
   b.call('notify')
 })
 
-tape('discard errors from remote for which we have no matching callback and error handler is not present', function (t) {
+tap('discard errors from remote for which we have no matching callback and error handler is not present', function (t) {
   t.plan(1)
-  a.on('error', onerror)
-  function onerror (err) {
+  a.addEventListener('error', onerror)
+  function onerror (evt) {
+    var err = evt.detail
     t.equal(err.message, 'bogus error')
-    a.removeListener('error', onerror)
+    a.removeEventListener('error', onerror)
     b.send({
       id: 999,
       error: {
